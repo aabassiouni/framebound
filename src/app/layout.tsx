cbd13635f17a894a6bf0ff47d40da6d72dc77717
aabassiouni <aabassiouni@hotmail.com>
2023-04-26 18:00:33 -0400

    added analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import './globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import { Inter, Roboto_Mono } from 'next/font/google';
+import { Inter, Roboto_Mono } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
     title: 'framebound',
@@ -11,25 +12,24 @@ export const metadata = {
 const inter = Inter({
     variable: '--font-inter',
     subsets: ['latin'],
-    display: 'swap'
-  });
+    display: 'swap',
+})
 
-  const roboto_mono = Roboto_Mono({
+const roboto_mono = Roboto_Mono({
     variable: '--font-roboto-mono',
     subsets: ['latin'],
-    display: 'swap'
-  });
-  
-  
+    display: 'swap',
+})
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
         <html lang="en" className={inter.className}>
-            <body className=''>
+            <body className="">
                 <Navbar />
                 {children}
                 <Footer />
+                <Analytics />
             </body>
-            
         </html>
     )
 }
